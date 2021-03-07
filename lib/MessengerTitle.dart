88c6eb7039a0e85daf669b82580bc2a8e5fe393c
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-03-07 14:35:01 +0900

    add messenger title

--- lib/MessengerTitle.dart
+++ lib/MessengerTitle.dart
@@ -1,8 +1,49 @@
 import "package:flutter/material.dart";
+import 'package:frontloops_03/constants.dart';
 
 class MessengerTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      height: kMessengerTitleHeight,
+      color: kMessengerTitleColor,
+      child: Padding(
+        padding: const EdgeInsets.all(kMessengerTitlePadding),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            RichText(
+              text: TextSpan(
+                children: [
+                  WidgetSpan(
+                    child: Transform.translate(
+                      offset: Offset(0, -8),
+                      child: Icon(
+                        Icons.circle,
+                        size: kMessengerTitleIconSize,
+                        color: kMessengerTitleTextColor,
+                      ),
+                    ),
+                  ),
+                  WidgetSpan(
+                    child: Text("   "),
+                  ),
+                  TextSpan(
+                    text: kMessengerTitle,
+                    style: TextStyle(
+                      fontSize: kMessengerFontSize,
+                      fontWeight: FontWeight.w700,
+                      letterSpacing: kMessengerLetterSpacing,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            kCloseButtonImage
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,23 @@
 import "package:flutter/material.dart";
 
 const String kAppTitle = "Frontloops 03";
+const String kMessengerTitle = "MARK ZUCKERBERG";
 
 const Color kBackgroundColor = Color.fromRGBO(178, 199, 215, 1);
 const Color kAppBarColor = Colors.blueAccent;
 const Color kContainerColor = Colors.white;
+const Color kMessengerTitleColor = Color.fromRGBO(96, 139, 211, 1);
+const Color kMessengerTitleTextColor = Colors.white;
 
+const double kMessengerTitlePadding = 30.0;
+const double kMessengerFontSize = 20.0;
+const double kMessengerLetterSpacing = 1.0;
+const double kMessengerTitleIconSize = 8.0;
+const double kMessengerTitleHeight = 80.0;
 const double kContainerTopMargin = 50.0;
 const double kContainerWidth = 350.0;
+
+final Widget kCloseButtonImage = Image.asset(
+  "./assets/images/close.png",
+  scale: 0.8,
+);
